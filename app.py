17e736b604763cc97d152e15eaaa858b6d3623e3
Thiago-Quixaba <thiagoquixaba78@gmail.com>
2025-10-05 20:54:24 -0300

    fingir que eu fiz alguma coisa

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-import flask
+import flask, database
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
     return flask.render_template("index.html")
+
+@app.route("/alunos")
+def alunos():
+    alunos = database.selectAll()
+    return flask.render_template("alunos.html", alunos=alunos)
+
+@app.route("/cadastro")
+def cadastro():
+    return flask.render_template("cadastro.html")
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    aluno = {
+        'name': flask.request.form.get("name"),
+        'birthdate': flask.request.form.get("birthdate"), 
+        'cpf': flask.request.form.get("cpf"),
+        'email': flask.request.form.get("email"),
+        'curso': flask.request.form.get("curso"),
+        'turma': flask.request.form.get("turma"),
+        'senha': flask.request.form.get("senha")
+        }
+
+    database.insert(aluno)
+
+    return flask.redirect("/")
 
 app.run(debug=True)
--- database.py
+++ database.py
@@ -4,3 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
+def selectAll():
+    alunos = supabase.table("alunos").select("*").execute()
+    
+    if alunos.data:
+        return alunos.data
+    else:
+        return []
+
+def insert(aluno: dict):
+    supabase.table("alunos").insert({
+        "name": aluno['name'],
+        "birthdate": aluno['birthdate'],
+        "cpf": aluno['cpf'],
+        "email": aluno['email'],
+        "curso": aluno['curso'],
+        "turma": aluno['turma'],
+        "senha": aluno['senha']
+    }).execute()
