b66da8990e9487fe37fa13c41825c6b5c62d1f0f
LigiaMenezes <lilimnz.2020@gmail.com>
2025-10-12 19:13:07 -0300

    Alunos

--- database.py
+++ database.py
@@ -40,6 +40,47 @@ def selectAllProfessores():
                             })
     return(professores)
 
+def insertAluno(aluno: dict):
+    encryption_key = Fernet.generate_key()
+    cipher = Fernet(encryption_key)
+
+    photo_url = aluno.get('photo_url')
+    if photo_url:
+        encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
+    else:
+        encrypted_photo = None
+
+    supabase.table('alunos').insert({
+        'encryption_key': encryption_key.decode(),
+        'password': cipher.encrypt(aluno['password'].encode()).decode(),
+        'name': aluno['name'],
+        'photo_url': encrypted_photo,
+        'birthdate': aluno['birthdate'],
+        'cpf': cipher.encrypt(aluno['cpf'].encode()).decode(),
+        'email': aluno['email'],
+        'course': aluno.get['course'],  
+        'class': aluno.get['class']           
+    }).execute()
+
+
+def selectAllAlunos():
+    alunos = []
+    for aluno in supabase.table('alunos').select('*').execute().data:
+        cipher = Fernet(aluno['encryption_key'])
+        alunos.append({
+            'id': aluno['id'],
+            'password': cipher.decrypt(aluno['password'].encode()).decode(),
+            'name': aluno['name'],
+            'photo_url': aluno['photo_url'],
+            'birthdate': aluno['birthdate'],
+            'cpf': cipher.decrypt(aluno['cpf'].encode()).decode(),
+            'email': aluno['email'],
+            'course': aluno.get['course'],
+            'class': aluno.get['class']
+        })
+    return alunos
+
+
 # insertProfessor({
 #     'password': "1234",
 #     'name': "Arthur",
@@ -47,3 +88,15 @@ def selectAllProfessores():
 #     'cpf': "12345678901",
 #     'email': "thiagoquixaba78@gmail.com"
 # })
+
+#--------------------------------------------
+
+#insertAluno({
+#    'password': "1234",
+#    'name': "Livia",
+#    'birthdate': "2025-11-02",
+#    'cpf': "12345678901",
+#    'email': "lili908@gmail.com",
+#    'course': "Informatica",
+#    'class': "3º"
+#})
