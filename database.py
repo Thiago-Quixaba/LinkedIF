cb81110513be41b2efaeea2f9dbb369f9cdbedbb
Thiago-Quixaba <thiagoquixaba78@gmail.com>
2025-10-12 20:57:10 -0300

    Atualização da Arquitetura de Criptografia

--- database.py
+++ database.py
@@ -5,83 +5,97 @@ import os
 
 load_dotenv()
 supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
+master_cipher = Fernet(os.getenv('MASTER_KEY').encode())
 
-def insertProfessor(professor: dict):
-    encryption_key = Fernet.generate_key()
-    cipher = Fernet(encryption_key)
+class Professores():
+    @staticmethod
+    def insert(professor: dict):
+        encryption_key = Fernet.generate_key()
+        cipher = Fernet(encryption_key)
 
-    photo_url = professor.get('photo_url')
-    if photo_url:
-        encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
-    else:
-        encrypted_photo = None
+        photo_url = professor.get('photo_url')
+        if photo_url:
+            encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
+        else:
+            encrypted_photo = None
 
-    supabase.table('professores').insert({
-        'encryption_key': encryption_key.decode(),
-        'password': cipher.encrypt(professor['password'].encode()).decode(),
-        'name': professor['name'],
-        'photo_url': encrypted_photo,
-        'birthdate': professor['birthdate'],
-        'cpf': cipher.encrypt(professor['cpf'].encode()).decode(),
-        'email': professor['email']
-    }).execute()
+        supabase.table('professores').insert({
+            'encryption_key': master_cipher.encrypt(encryption_key).decode(),
+            'password': cipher.encrypt(professor['password'].encode()).decode(),
+            'name': professor['name'],
+            'photo_url': encrypted_photo,
+            'birthdate': professor['birthdate'],
+            'cpf': cipher.encrypt(professor['cpf'].encode()).decode(),
+            'email': professor['email']
+        }).execute()
 
-def selectAllProfessores():
-    professores = []
-    for professor in supabase.table('professores').select('*').execute().data:
-        cipher = Fernet(professor['encryption_key'])
-        professores.append({'id': professor['id'], 
-                            'password': cipher.decrypt(professor['password'].encode()).decode(),
-                            'name': professor['name'],
-                            'photo_url': professor['photo_url'],
-                            'birthdate': professor['birthdate'],
-                            'cpf': cipher.decrypt(professor['cpf'].encode()).decode(),
-                            'email': professor['email']
-                            })
-    return(professores)
+    @staticmethod
+    def selectAll():
+        professores = []
+        for professor in supabase.table('professores').select('*').execute().data:
+            cipher = Fernet(master_cipher.decrypt(professor['encryption_key'].encode()).decode())
+            if professor['photo_url']:
+                decrypted_photo = cipher.decrypt(professor['photo_url'].encode()).decode()
+            else:
+                decrypted_photo = None
+            professores.append({'id': professor['id'], 
+                                'password': cipher.decrypt(professor['password'].encode()).decode(),
+                                'name': professor['name'],
+                                'photo_url': decrypted_photo,
+                                'birthdate': professor['birthdate'],
+                                'cpf': cipher.decrypt(professor['cpf'].encode()).decode(),
+                                'email': professor['email']
+                                })
+        return(professores)
 
-def insertAluno(aluno: dict):
-    encryption_key = Fernet.generate_key()
-    cipher = Fernet(encryption_key)
+class Alunos():
+    @staticmethod
+    def insert(aluno: dict):
+        encryption_key = Fernet.generate_key()
+        cipher = Fernet(encryption_key)
 
-    photo_url = aluno.get('photo_url')
-    if photo_url:
-        encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
-    else:
-        encrypted_photo = None
+        photo_url = aluno.get('photo_url')
+        if photo_url:
+            encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
+        else:
+            encrypted_photo = None
 
-    supabase.table('alunos').insert({
-        'encryption_key': encryption_key.decode(),
-        'password': cipher.encrypt(aluno['password'].encode()).decode(),
-        'name': aluno['name'],
-        'photo_url': encrypted_photo,
-        'birthdate': aluno['birthdate'],
-        'cpf': cipher.encrypt(aluno['cpf'].encode()).decode(),
-        'email': aluno['email'],
-        'course': aluno.get('course'),  
-        'class': aluno.get('class')           
-    }).execute()
-
-
-def selectAllAlunos():
-    alunos = []
-    for aluno in supabase.table('alunos').select('*').execute().data:
-        cipher = Fernet(aluno['encryption_key'])
-        alunos.append({
-            'id': aluno['id'],
-            'password': cipher.decrypt(aluno['password'].encode()).decode(),
+        supabase.table('alunos').insert({
+            'encryption_key': master_cipher.encrypt(encryption_key).decode(),
+            'password': cipher.encrypt(aluno['password'].encode()).decode(),
             'name': aluno['name'],
-            'photo_url': aluno['photo_url'],
+            'photo_url': encrypted_photo,
             'birthdate': aluno['birthdate'],
-            'cpf': cipher.decrypt(aluno['cpf'].encode()).decode(),
+            'cpf': cipher.encrypt(aluno['cpf'].encode()).decode(),
             'email': aluno['email'],
-            'course': aluno.get('course'),
-            'class': aluno.get('class')
-        })
-    return alunos
+            'course': aluno.get('course'),  
+            'class': aluno.get('class')           
+        }).execute()
+
+    @staticmethod
+    def selectAll():
+        alunos = []
+        for aluno in supabase.table('alunos').select('*').execute().data:
+            cipher = Fernet(master_cipher.decrypt(aluno['encryption_key'].encode()).decode())
+            if aluno['photo_url']:
+                decrypted_photo = cipher.decrypt(aluno['photo_url'].encode()).decode()
+            else:
+                decrypted_photo = None
+            alunos.append({
+                'id': aluno['id'],
+                'password': cipher.decrypt(aluno['password'].encode()).decode(),
+                'name': aluno['name'],
+                'photo_url': decrypted_photo,
+                'birthdate': aluno['birthdate'],
+                'cpf': cipher.decrypt(aluno['cpf'].encode()).decode(),
+                'email': aluno['email'],
+                'course': aluno.get('course'),
+                'class': aluno.get('class')
+            })
+        return alunos
 
 
-# insertProfessor({
+# Professores.insert({
 #     'password': "1234",
 #     'name': "Arthur",
 #     'birthdate': "2025-11-02",
@@ -89,14 +103,17 @@ def selectAllAlunos():
 #     'email': "thiagoquixaba78@gmail.com"
 # })
 
-#--------------------------------------------
+# #--------------------------------------------
 
-insertAluno({
-    'password': "1234",
-    'name': "Livia",
-    'birthdate': "2025-11-02",
-    'cpf': "12345678901",
-    'email': "lili908@gmail.com",
-    'course': "Informatica",
-    'class': "3º"
-})
+# Alunos.insert({
+#     'password': "1234",
+#     'name': "Livia",
+#     'birthdate': "2025-11-02",
+#     'cpf': "12345678901",
+#     'email': "lili908@gmail.com",
+#     'course': "Informatica",
+#     'class': "3"
+# })
+# print(Alunos.selectAll())
+# print("-----------------------------------------------------------------")
+# print(Professores.selectAll())
