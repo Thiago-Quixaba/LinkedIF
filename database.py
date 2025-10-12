ff8f5ac616033d0c0ed9bbf03bf7d02115e05476
Thiago-Quixaba <thiagoquixaba78@gmail.com>
2025-10-11 22:49:17 -0300

    integração inicial com o banco de dados supabase

--- database.py
+++ database.py
@@ -1,25 +1,49 @@
-import os
+from cryptography.fernet import Fernet
 from supabase import create_client, Client
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
-def selectAll():
-    alunos = supabase.table("alunos").select("*").execute()
-    
-    if alunos.data:
-        return alunos.data
+def insertProfessor(professor: dict):
+    encryption_key = Fernet.generate_key()
+    cipher = Fernet(encryption_key)
+
+    photo_url = professor.get('photo_url')
+    if photo_url:
+        encrypted_photo = cipher.encrypt(photo_url.encode()).decode()
     else:
-        return []
+        encrypted_photo = None
 
-def insert(aluno: dict):
-    supabase.table("alunos").insert({
-        "name": aluno['name'],
-        "birthdate": aluno['birthdate'],
-        "cpf": aluno['cpf'],
-        "email": aluno['email'],
-        "curso": aluno['curso'],
-        "turma": aluno['turma'],
-        "senha": aluno['senha']
+    supabase.table('professores').insert({
+        'encryption_key': encryption_key.decode(),
+        'password': cipher.encrypt(professor['password'].encode()).decode(),
+        'name': professor['name'],
+        'photo_url': encrypted_photo,
+        'birthdate': professor['birthdate'],
+        'cpf': cipher.encrypt(professor['cpf'].encode()).decode(),
+        'email': professor['email']
     }).execute()
+
+def selectAllProfessores():
+    professores = []
+    for professor in supabase.table('professores').select('*').execute().data:
+        cipher = Fernet(professor['encryption_key'])
+        professores.append({'id': professor['id'], 
+                            'password': cipher.decrypt(professor['password'].encode()).decode(),
+                            'name': professor['name'],
+                            'photo_url': professor['photo_url'],
+                            'birthdate': professor['birthdate'],
+                            'cpf': cipher.decrypt(professor['cpf'].encode()).decode(),
+                            'email': professor['email']
+                            })
+    return(professores)
+
+# insertProfessor({
+#     'password': "1234",
+#     'name': "Arthur",
+#     'birthdate': "2025-11-02",
+#     'cpf': "12345678901",
+#     'email': "thiagoquixaba78@gmail.com"
+# })
