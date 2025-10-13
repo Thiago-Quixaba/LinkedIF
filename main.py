d3797d49678f66632d80b37713108fb7a6925156
Thiago-Quixaba <thiagoquixaba78@gmail.com>
2025-10-13 16:40:43 -0300

    Implementação de cadastro de professores

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ app = flask.Flask(__name__)
 @app.route("/")
 def home():
     return flask.render_template("index.html")
-
+#|-----------------Cadastro-------------------|
 @app.route("/cadastro")
 def cadastro():
     return flask.render_template("cadastro.html")
+
+#|--------------Cadastro Aluno----------------|
 
 @app.route("/cadastro/aluno")
 def cadastro_aluno():
     return flask.render_template("cadastro/aluno.html")
 
 @app.route("/cadastrarAluno", methods=["POST"])
-def cadastrar():
+def cadastrarAluno():
     aluno = {
         'name': flask.request.form.get("name"),
         'birthdate': flask.request.form.get("birthdate"), 
@@ -28,6 +30,27 @@ def cadastrar():
 
 
     database.Alunos.insert(aluno)
+
+    return flask.redirect("/")
+
+#|-------------Cadastro Professor---------------|
+
+@app.route("/cadastro/professor")
+def cadastro_professor():
+    return flask.render_template("cadastro/professor.html")
+
+@app.route("/cadastrarProfessor", methods=["POST"])
+def cadastrarProfessor():
+    professor = {
+        'name': flask.request.form.get("name"),
+        'birthdate': flask.request.form.get("birthdate"), 
+        'cpf': flask.request.form.get("cpf"),
+        'email': flask.request.form.get("email"),
+        'password': flask.request.form.get("password")
+        }
+
+
+    database.Professores.insert(professor)
 
     return flask.redirect("/")
 
