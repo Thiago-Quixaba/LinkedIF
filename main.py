9247c38ae58532f02c011a8940896ff69a090f0e
Thiago-Quixaba <thiagoquixaba78@gmail.com>
2025-12-09 00:45:55 -0300

    Atualização de Segurança

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'linkedifpi@gmail.com'
-app.config['MAIL_PASSWORD'] = 'hjgm wmgz ueuf xnvj '
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 
